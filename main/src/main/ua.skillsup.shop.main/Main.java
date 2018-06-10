5220f3d649169dd2f501db1075140c56cdeb296d
Alexey <archon@ua.fm>
2018-06-10 14:55:31 +0300

    commit

--- main/src/main/ua.skillsup.shop.main/Main.java
+++ main/src/main/ua.skillsup.shop.main/Main.java
@@ -2,7 +2,9 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 import ua.skillsup.shop.services.ProductService;
 import ua.skillsup.shop.services.UserService;
+import ua.skillsup.shop.services.dto.ProductDto;
 import ua.skillsup.shop.services.dto.UserDto;
+import ua.skillsup.shop.services.impl.UserServiceImpl;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -26,13 +28,25 @@ public class Main {
             String input = reader.readLine();
             String [] arguments = input.split( " ");
 
-            if ("C".equals(arguments[0])) {
-                UserDto dto = new UserDto(arguments[1], arguments[2], Integer.parseInt(arguments[3]));
-                userService.create(dto);
+            if ("UC".equals(arguments[0])) {
+                UserDto dtoUser = new UserDto(arguments[1], arguments[2], Integer.parseInt(arguments[3]));
+                userService.create(dtoUser);
                 System.out.println("User has been created");
-            }else if ("L".equals(arguments[0])){
+            }
+            if ("UL".equals(arguments[0])){
                 System.out.println("List of users");
                 System.out.println(userService.findAll());
+            }
+            if ("PC".equals(arguments[0])) {
+                ProductDto dtoProduct = new ProductDto(arguments[1], Double.parseDouble(arguments[2]), arguments[3], arguments[4], arguments[5], Double.parseDouble(arguments[6]), Integer.parseInt(arguments[7]));
+                productService.create(dtoProduct);
+                System.out.println("Product has been created");
+            }
+
+            if ("PL".equals(arguments[0])) {
+                System.out.println("List of products");
+                System.out.println(productService.findAll());
+
             }
         }
 
